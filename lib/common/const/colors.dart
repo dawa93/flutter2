3b33aabd6e8af9d66aa4ee41b910ff6c08ccfc20
Hwanieee <dawa9293@gmail.com>
2023-03-28 10:05:45 +0900

    chore(setting): 글러벌 컬러 추가

---
+++ lib/common/const/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+// main color
+const PRIMARY_COLOR = Color(0xFF22A45D);
+
+// text color
+const BODY_TEXT_COLOR = Color(0xFF868686);
+
+// textfield  background color
+const INPUT_BG_COLOR = Color(0xFFFBFBFB);
+
+// textfield border color
+const INPUT_BORDER_COLOR = Color(0xFFF3F2F2);
