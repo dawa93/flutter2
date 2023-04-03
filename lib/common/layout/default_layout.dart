5a91e4bcb6e2a69cb8e99a333621d405c720bd06
Donghwan <Hwaniee@Katies-MacBook-Pro.local>
2023-04-04 00:59:24 +0900

    탭바 ui 추가

--- lib/common/layout/default_layout.dart
+++ lib/common/layout/default_layout.dart
@@ -2,19 +2,41 @@ import 'package:flutter/material.dart';
 
 class DefaultLayout extends StatelessWidget {
   final Widget child;
+  final Widget? bottomNavigationbar;
   final Color? backgroundColor;
+  final String? title;
 
   const DefaultLayout({
     super.key,
     required this.child,
     this.backgroundColor,
+    this.title,
+    this.bottomNavigationbar,
   });
+
+  AppBar? renderAppBar() {
+    if (title == null) {
+      return null;
+    } else {
+      return AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        foregroundColor: Colors.black, // app bar위에 올라가는 것들의 색상을 한번에 정하는 방법
+        title: Text(
+          title!,
+          style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor ?? Colors.white,
+      appBar: renderAppBar(),
       body: child,
+      bottomNavigationBar: bottomNavigationbar,
     );
   }
 }
